8a95f82fa8096fd9e66cdf598a2cab0f3727457a
shanxuecheng <shanxuecheng@gmail.com>
2014-05-21 19:40:53 +0800

    StopAnalyzer2

---
+++ src/test/java/com/sxc/lucene/analysis/StopAnalyzer2.java
@@ -0,0 +1,37 @@
+package com.sxc.lucene.analysis;
+
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LetterTokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.StopFilter;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.util.Version;
+
+public class StopAnalyzer2 extends Analyzer {
+
+	private CharArraySet stopWords;
+
+	public StopAnalyzer2() {
+		stopWords = StopAnalyzer.ENGLISH_STOP_WORDS_SET;
+	}
+
+	public StopAnalyzer2(String[] stopWords) {
+		this.stopWords = StopFilter.makeStopSet(Version.LUCENE_47, stopWords);
+	}
+
+	@Override
+	protected TokenStreamComponents createComponents(String fieldName,
+			Reader reader) {
+		Tokenizer source = new LetterTokenizer(Version.LUCENE_47, reader);
+		TokenStream tokenStream = new StopFilter(Version.LUCENE_47,
+				new LowerCaseFilter(Version.LUCENE_47, source), stopWords);
+
+		return new TokenStreamComponents(source, tokenStream);
+	}
+
+}
