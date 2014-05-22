fa499d80a3a30ef82ca05b41cfe814d8ea03633d
shanxuecheng <shanxuecheng@gmail.com>
2014-05-22 09:57:49 +0800

    testStopAnalyzer2 | testStopAnalyzerFlawed

--- src/test/java/com/sxc/lucene/analysis/AnalyzerDemo.java
+++ src/test/java/com/sxc/lucene/analysis/AnalyzerDemo.java
@@ -13,9 +13,11 @@ package com.sxc.lucene.analysis;
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific lan      
-*/
+ */
 
 import java.io.IOException;
+
+import junit.framework.TestCase;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
@@ -24,51 +26,57 @@ import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.util.Version;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 // From chapter 4
 
 /**
- * Adapted from code which first appeared in a java.net article
- * written by Erik
+ * Adapted from code which first appeared in a java.net article written by Erik
  */
-public class AnalyzerDemo {
-  private static final String[] examples = {
-    "The quick brown fox jumped over the lazy dog",
-    "XY&Z Corporation - xyz@example.com",
-    "Amsterdam Venice",
-    "北京 南通"
-  };
+public class AnalyzerDemo extends TestCase {
+	private static final String[] examples = {
+			"The quick brown fox jumped over the lazy dog",
+			"XY&Z Corporation - xyz@example.com", "Amsterdam Venice", "北京 南通市" };
 
-  private static final Analyzer[] analyzers = new Analyzer[] { 
-    new WhitespaceAnalyzer(Version.LUCENE_47),
-    new SimpleAnalyzer(Version.LUCENE_47),
-    new StopAnalyzer(Version.LUCENE_47),
-    new StandardAnalyzer(Version.LUCENE_47),
-    new SmartChineseAnalyzer(Version.LUCENE_47)
-  };
+	private static final Analyzer[] analyzers = new Analyzer[] {
+			new WhitespaceAnalyzer(Version.LUCENE_47),
+			new SimpleAnalyzer(Version.LUCENE_47),
+			new StopAnalyzer(Version.LUCENE_47),
+			new StandardAnalyzer(Version.LUCENE_47),
+			new SmartChineseAnalyzer(Version.LUCENE_47), new IKAnalyzer() };
 
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-    String[] strings = examples;
-    if (args.length > 0) {    // A
-      strings = args;
-    }
+		String[] strings = examples;
+		if (args.length > 0) { // A
+			strings = args;
+		}
 
-    for (String text : strings) {
-      analyze(text);
-    }
-  }
+		for (String text : strings) {
+			analyze(text);
+		}
+	}
 
-  private static void analyze(String text) throws IOException {
-    System.out.println("Analyzing \"" + text + "\"");
-    for (Analyzer analyzer : analyzers) {
-      String name = analyzer.getClass().getSimpleName();
-      System.out.println("  " + name + ":");
-      System.out.print("    ");
-      AnalyzerUtils.displayTokens(analyzer, text); // B
-      System.out.println("\n");
-    }
-  }
+	private static void analyze(String text) throws IOException {
+		System.out.println("Analyzing \"" + text + "\"");
+		for (Analyzer analyzer : analyzers) {
+			String name = analyzer.getClass().getSimpleName();
+			System.out.println("  " + name + ":");
+			System.out.print("    ");
+			AnalyzerUtils.displayTokens(analyzer, text); // B
+			System.out.println("\n");
+		}
+	}
+
+	public void testStopAnalyzer2() throws Exception {
+		AnalyzerUtils.assertAnalyzesTo(new StopAnalyzer2(),
+				"The quick brown...", new String[] { "quick", "brown" });
+	}
+
+	public void testStopAnalyzerFlawed() throws Exception {
+		AnalyzerUtils.assertAnalyzesTo(new StopAnalyzerFlawed(),
+				"The quick brown...", new String[] { "the", "quick", "brown" });
+	}
 }
 
 // #A Analyze command-line strings, if specified
